4bc19509e8e0504a8c781c6c5d61d00dfaaf99c2
MinRi2 <minri2@qq.com>
2025-12-27 13:46:02 +0800

    🚑️ Steam快捷键为空

--- src/dpsmap/Main.java
+++ src/dpsmap/Main.java
@@ -14,32 +14,30 @@ public class Main extends Mod{
             DpsKeyBinds.init();
             DpsShaders.init();
 
-//            showDpsDialog();
-        });
+            Events.run(Trigger.update, () -> {
+                if(!Core.scene.hasField()){
+                    if(Core.input.keyRelease(DpsKeyBinds.enable)){
+                        String key = DpsSettings.enable.toggle() ? "setting.enable" : "setting.disable";
+                        String info = Core.bundle.format(key, DpsSettings.enable.localized());
+                        Vars.ui.showInfoFade(info);
+                    }
 
-        Events.run(Trigger.update, () -> {
-            if(!Core.scene.hasField()){
-                if(Core.input.keyRelease(DpsKeyBinds.enable)){
-                    String key = DpsSettings.enable.toggle() ? "setting.enable" : "setting.disable";
-                    String info = Core.bundle.format(key, DpsSettings.enable.localized());
-                    Vars.ui.showInfoFade(info);
+                    if(Core.input.keyRelease(DpsKeyBinds.showPlayerTeam)){
+                        String key = DpsSettings.showPlayerTeam.toggle() ? "setting.enable" : "setting.disable";
+                        String info = Core.bundle.format(key, DpsSettings.showPlayerTeam.localized());
+                        Vars.ui.showInfoFade(info);
+                    }
+
+                    if(Core.input.keyRelease(DpsKeyBinds.nextTargetMode)){
+                        int next = (DpsSettings.targetMode.get() + 1) % DpsTargetMode.all.length;
+                        DpsTargetMode nextMode = DpsTargetMode.all[next];
+                        DpsSettings.targetMode.set(next);
+                        Vars.ui.showInfoFade(Core.bundle.format("dpsHeatmap.nextTargetMode", nextMode.localized()));
+                    }
                 }
 
-                if(Core.input.keyRelease(DpsKeyBinds.showPlayerTeam)){
-                    String key = DpsSettings.showPlayerTeam.toggle() ? "setting.enable" : "setting.disable";
-                    String info = Core.bundle.format(key, DpsSettings.showPlayerTeam.localized());
-                    Vars.ui.showInfoFade(info);
-                }
-
-                if(Core.input.keyRelease(DpsKeyBinds.nextTargetMode)){
-                    int next = (DpsSettings.targetMode.get() + 1) % DpsTargetMode.all.length;
-                    DpsTargetMode nextMode = DpsTargetMode.all[next];
-                    DpsSettings.targetMode.set(next);
-                    Vars.ui.showInfoFade(Core.bundle.format("dpsHeatmap.nextTargetMode", nextMode.localized()));
-                }
-            }
-
-            DpsHeatmap.update();
+                DpsHeatmap.update();
+            });
         });
 
         Events.run(Trigger.postDraw, DpsHeatmap::draw);
